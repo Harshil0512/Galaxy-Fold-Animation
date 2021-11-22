8c3c6ca9a8553f3a7ca1b8ee1186455b76ac1776
Harshil Khamar <73790584+Harshil0512@users.noreply.github.com>
2021-11-22 18:50:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let content = document.querySelectorAll('#content');
+let container = document.querySelectorAll("#container");
+content[0].addEventListener("scroll",()=>{
+    let fig = document.querySelector('figure');
+    let scroll=content[0].scrollTop;
+    if(scroll<=200)
+    { 
+        content[0].children[0].setAttribute("style",`transform:translate(0px,${scroll}px); opacity:1;`);
+        content[0].children[1].setAttribute("style",`transform:translate(0px,${scroll}px); opacity:${scroll/200};`);
+    }
+    else if(scroll>200 && scroll<400)
+    {
+        
+        content[0].children[0].setAttribute("style",`opacity:0;`);
+        content[0].children[1].setAttribute("style",`transform:translate(0px,${scroll}px); opacity:1;`);
+    }
+    else if(scroll>=400 && scroll<600)
+    {
+        content[0].children[1].setAttribute("style",`transform:translate(0px,${scroll}px); opacity:1;`);
+        content[0].setAttribute("style",`border-radius:${(scroll-400)/2}px;transform: scale(${1-(scroll-400)*0.0035});`);
+    }
+    else if(scroll>=600 && scroll<800)
+    {
+        content[0].children[1].setAttribute("style",`transform:translate(${600-(scroll)}px,${scroll}px); opacity:1;`);
+        content[0].setAttribute("style",`transform:scale(${0.3});border-width:${(scroll-600)*1.35}px ${(scroll-600)*2.43}px;border-color:transparent;border-style:none; border-radius:100px;`);
+    }
+    else if(scroll>=800 && scroll<=1000)
+    {
+        content[0].children[1].setAttribute("style",`transform:translate(-200px,${scroll}px); opacity:1;`);
+        content[0].children[1].children[1].setAttribute("style",`opacity:${(scroll-800)/200};`);
+    }
+    else
+    {
+        content[0].children[1].setAttribute("style",`transform:translate(-200px,${scroll}px); opacity:1; overflow:none;`);
+    }
+    console.log(scroll);
+});
